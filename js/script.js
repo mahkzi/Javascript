a5fcb11658171e4070c14784197ec92d9314d8c9
Castro Máximo <maximocastrobraga@gmail.com>
2026-04-21 14:25:45 -0300

    Micordesafio semana 2 completado

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
-let booleano = "true";
-let numero = 10;
-let string = "Primer String";
-let mensaje = "Hola mundo";
+// let edad = parseFloat(prompt("Ingrese su edad:"));
+//  if (edad < 4){
+//     alert("No viviste ningun mundial")
+//  }else if (edad >= 4 && edad < 8){
+//     alert("Viviste un mundial")
+//  }else if (edad >=8  && edad < 12){
+//     alert("Viviste dos mundiales")
+//  }else if (edad >=12  && edad < 16){
+//     alert("Viviste tres mundiales")
+//  }else if (edad >=16  && edad < 20){
+//     alert("Viviste 4 mundiales")
+//  }else if (edad >=20  && edad < 24){
+//     alert("Viviste 5 mundiales")
+//  }else if (edad >=24  && edad < 28){
+//     alert("Viviste 6 mundiales")
+//  }else if (edad >=28  && edad < 32){
+//     alert("Viviste 7 mundiales")
+//  }else if (edad >=32  && edad < 36){
+//     alert("Viviste 8 mundiales")
+//  }else if (edad >=36  && edad < 40){
+//     alert("Viviste 9 mundiales")
+//  }else{
+//     alert("Viviste todos los mundiales")
+//  };
+
+
